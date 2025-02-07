e74944cc81f41792ba76ff6113bec2a8d67812c1
Pyra <oddball80@hotmail.com>
2025-02-07 17:57:18 -0500

    Projectile to Stone

---
+++ src/Flares.cs
@@ -0,0 +1,15 @@
+﻿using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+
+namespace Flares
+{
+    public class Flares : EntityThrownStone
+    {
+        public Flares() 
+        {
+            this.HorizontalImpactBreakChance = 0;
+        }
+
+    }
+}
